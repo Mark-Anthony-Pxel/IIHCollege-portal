726560ddec3a6d6b347daea83fa85c7905406e57
Pixel <pixelmain13@gmail.com>
2024-11-12 20:37:45 +0800

    Fix system

--- iihc_website_portal/settings.py
+++ iihc_website_portal/settings.py
@@ -71,12 +71,12 @@ WSGI_APPLICATION = 'iihc_website_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': dj_database_url.config(
